7d4ca0ca13a0b1915a1e61e52b5d7f85ee6bee2b
Pham Thanh Tan <mrthanhtan22@gmail.com>
2016-08-14 22:49:20 +0700

    new

---
+++ js/jquery.js
@@ -0,0 +1,15 @@
+			$(document).ready(function() {
+ 			$("#btn-dichvu").hover(function(){
+       		$("#icon").toggleClass('doimau');
+    });		
+ 			$("#btn-dichvu2").hover(function(){
+       		$("#icon2").toggleClass('doimau');
+    });	
+ 			$("#btn-dichvu3").hover(function(){
+       		$("#icon3").toggleClass('doimau');
+    });	
+ 			$("#btn-dichvu4").hover(function(){
+       		$("#icon4").toggleClass('doimau');
+    });	
+ 		});
+			
